5e5ec4c93c92b833446f706b718388d3d276b9e0
occiandiaali <ocean@localhost.localdomain>
2020-06-12 11:54:20 +0100

    implement switch set on checked change listeners and failed to solve bug with regenerated password on switch unchecked

--- app/src/main/java/com/occian/passworddroid/MainActivity.kt
+++ app/src/main/java/com/occian/passworddroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.occian.passworddroid
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -15,8 +16,13 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
-    var temp: Int = 0
-
+    private var password = ""
+    private var passLength: Int = 0
+    private var numOfSwitch: Int = 0
+    private val caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    private val cops = "abcdefghijklmnopqrstuvwxyz"
+    private val nums = "0123456789"
+    private val syms = "@#!&$%_+?<>"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,44 +38,124 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         }
         spinner.onItemSelectedListener = this
 
+        switchLowers.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                password += cops
+                numOfSwitch += 1
+            } else {
+                password = password.removeSuffix(cops)
+                passwdTextView.text = "Password display here"
+                verdictTextView.text = "PW Verdict"
+                verdictTextView.setTextColor(Color.DKGRAY)
+                progressBar.progress = 0
+                numOfSwitch -= 1
+            }
+        }
+        switchUppers.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                password += caps
+                numOfSwitch += 1
+            } else {
+                password = password.removeSuffix(caps)
+                passwdTextView.text = "Password display here"
+                verdictTextView.text = "PW Verdict"
+                verdictTextView.setTextColor(Color.DKGRAY)
+                progressBar.progress = 0
+                numOfSwitch -= 1
+            }
+        }
+        switchNumbers.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                password += nums
+                numOfSwitch += 1
+            } else {
+                password = password.removeSuffix(nums)
+                passwdTextView.text = "Password display here"
+                verdictTextView.text = "PW Verdict"
+                verdictTextView.setTextColor(Color.DKGRAY)
+                progressBar.progress = 0
+                numOfSwitch -= 1
+            }
+        }
+        switchSymbols.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                password += syms
+                numOfSwitch += 1
+            } else {
+                password = password.removeSuffix(syms)
+                passwdTextView.text = "Password display here"
+                verdictTextView.text = "PW Verdict"
+                verdictTextView.setTextColor(Color.DKGRAY)
+                progressBar.progress = 0
+                numOfSwitch -= 1
+            }
+        }
+
         // click listener that fires the generate pw function
-        floatingActionButtonRun.setOnClickListener { generatePW(temp) }
+        floatingActionButtonRun.setOnClickListener {
+            if (numOfSwitch < 1) {
+                Toast.makeText(applicationContext, "Select at least ONE switch", Toast.LENGTH_SHORT).show()
+            } else {
+                //password = ""
+                passwdTextView.text = "Password display here"
+                verdictTextView.text = "PW Verdict"
+                verdictTextView.setTextColor(Color.DKGRAY)
+                progressBar.progress = 0
+            }
+
+                generatePW(passLength)
+        } // fab
+
     } // on create
 
     private fun generatePW(len: Int): String {
-        // initialize the base components
-        val caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        val cops = "abcdefghijklmnopqrstuvwxyz"
-        val nums = "0123456789"
-        val syms = "@#!&$%_+?<>"
-
-        // string them together
-        val values = caps + syms + cops + nums
         val sb = StringBuilder(len)
-
-        // initialize the random generator
+        passwordStrength() // calls function to set progressbar colour
+        // initialize the random generator and randomize the concats
         val rand = Random
-
         if (len >= 8) {
             for (i in 0..len) {
-                var index = (values.length * Math.random()).toInt()
+                var index = (password.length * Math.random()).toInt()
                 try {
-                    sb.append(values[index])
+                    sb.append(password[index])
                 } catch (e: Exception) {
 
                 }
             }
         }
+
         passwdTextView.text = sb.toString()
         return passwdTextView.toString()
     } // generate pw func
+
+    private fun passwordStrength() {
+        if (numOfSwitch == 0){
+            progressBar.progress = 0
+        }
+        if (passLength == 8 && numOfSwitch >= 1) {
+            progressBar.progress = 50
+            verdictTextView.text = "Good"
+            verdictTextView.setTextColor(Color.parseColor("#ccce0f"))
+        }
+        if (passLength == 16 && numOfSwitch >= 1) {
+            progressBar.progress = 75
+            verdictTextView.text = "Strong"
+            verdictTextView.setTextColor(Color.parseColor("#98c807"))
+        }
+        if (passLength == 32 && numOfSwitch >= 1) {
+            progressBar.progress = 100
+            verdictTextView.text = "Stronger"
+            verdictTextView.setTextColor(Color.parseColor("#969600"))
+        }
+
+    } // pw strength func
 
     // for spinner adapter
     override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
         val text: String = parent?.getItemAtPosition(pos).toString()
-        temp = text.toInt()
+        passLength = text.toInt()
     }
 
     override fun onNothingSelected(parent: AdapterView<*>) {
